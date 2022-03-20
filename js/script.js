a65936a91a3049d51e9df1e2952dc253da32ea84
Viktoriya.Khilko <svictoria951@gmail.com>
2022-03-20 16:48:55 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+/*let number = 5;
+const leftBorderWidth = 1;
+
+//number = 10;
+//console.log(number);
+
+const obj = {
+   name: 'John',
+   age: 25,
+   isMarried: false
+};
+
+
+//console.log(obj);
+
+let arr = [
+    'orange.png',
+    6,
+    obj,
+    leftBorderWidth
+];
+alert(arr);
+
+
+const result = confirm ("I ask you something.");
+console.log(result);
+
+
+const answer = prompt ("Are you 18??", "17s");
+console.log(answer);
+*/
+
+const answers = [];
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('What is your age?', '');
+answers[2] = prompt('What is your what?', '');
+
+document.write(answers);
