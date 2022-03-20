40e0c6c5e1ca806a6b9bf28f6769dbac30ede95c
Viktoriya.Khilko <svictoria951@gmail.com>
2022-03-20 18:55:31 +0200

    Add 2 questions and console log

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //1
 const numberOfFilms = +prompt ("How many films did you already see?", ""); //+prompt if we need only numbers? not string
-console.log(numberOfFilms);
+
 
 //2
 const personalMovieDB = {
@@ -10,3 +10,14 @@ actors: {},
 genders: [],
 privat: false
 };
+
+//3
+const lastFilm = prompt ("Print one of the last watched films", ""),
+      lastFilmGrade = prompt ("Print the grade of this film:", ""),
+      lastFilm2 = prompt ("Print one of the last watched films", ""),
+      lastFilmGrade2 = prompt ("Print the grade of this film:", "");
+
+personalMovieDB.movies[lastFilm] = lastFilmGrade;
+personalMovieDB.movies[lastFilm2] = lastFilmGrade2;
+
+console.log(personalMovieDB);
