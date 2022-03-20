7d6b5a977ee5a4bde91571b2edce3c284b039878
Viktoriya.Khilko <svictoria951@gmail.com>
2022-03-20 18:42:06 +0200

    add personalMovieDB object

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
-const numberOfFilms = prompt ("How many films did you already see?", "");
+//1
+const numberOfFilms = +prompt ("How many films did you already see?", ""); //+prompt if we need only numbers? not string
 console.log(numberOfFilms);
+
+//2
+const personalMovieDB = {
+count: numberOfFilms,
+movies: {},
+actors: {},
+genders: [],
+privat: false
+};
