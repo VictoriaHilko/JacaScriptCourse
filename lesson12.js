653136e9b39be995b26ad25716c13d2373b22549
Viktoriya.Khilko <svictoria951@gmail.com>
2022-03-22 18:52:44 +0200

    Add lesson12.js

---
+++ lesson12.js
@@ -0,0 +1,32 @@
+"use strict";
+//1
+const num = 50;
+
+if(num < 49){
+    console.log('Less');
+} else if (num > 100){
+    console.log('More');
+} else {
+    console.log('Ok');
+    }
+
+(num == 50) ? console.log('Ok') : console.log('Error');
+
+//2
+
+const count = 10
+
+switch(count){
+    case 5:
+        console.log('Less');
+        break;
+    case 10:
+        console.log('OK');
+        break;  
+    case 15:
+        console.log('More');
+        break;   
+    default:
+        console.log('Ne srabotalo ni odno iz usloviy');
+        break;   
+}
