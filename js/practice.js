d1e3837199f00b8f08cdecf323fc16cefdceba62
Viktoriya.Khilko <svictoria951@gmail.com>
2022-03-22 19:59:18 +0200

    Update practice.js

--- js/practice.js
+++ js/practice.js
@@ -10,10 +10,27 @@ privat: false
 }; 
 
 
-for(let i=0; i<3; i++){
-const lastFilm = prompt ('Print one of the last watched films', ''),
-      lastFilmGrade = prompt ('Print the grade of this film:', '');
+for(let i=0; i<2; i++){
+    const lastFilm = prompt ('Print one of the last watched films', ''),
+          lastFilmGrade = prompt ('Print the grade of this film:', '');
+ 
+    if (lastFilm != null && lastFilmGrade != null  && lastFilm != '' && lastFilmGrade != null && lastFilm.length < 50){
+        personalMovieDB.movies[lastFilm] = lastFilmGrade;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    }
 
-personalMovieDB.movies[lastFilm] = lastFilmGrade;
+if (numberOfFilms < 10){
+    document.write('Malo');
+} else if (numberOfFilms >= 10 && numberOfFilms < 30){
+    document.write('Norm');
+} else if (numberOfFilms >= 30){
+    document.write('Mnogo');
+} else {
+    document.write('Error');
 }
-console.log(personalMovieDB);
+
+console.log(personalMovieDB); 
